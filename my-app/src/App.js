908ba54dcaf34bc2337a412a2f017b0d5c07296a
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-04-13 23:32:56 -0700

    set up router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import InputForm from './InputForm';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <InputForm />
+            <main>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/form" component={InputForm} />
+                </Switch>
+            </main>
         );
     }
 }
